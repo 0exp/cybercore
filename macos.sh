f237b9de701e7aa7177afab227a0130856d174c3
Rustam Ibragimov <iamdaiver@icloud.com>
2018-07-08 17:09:51 +0300

    [macos] mkcert (locally trusted development certificates)

--- macos.sh
+++ macos.sh
@@ -15,24 +15,14 @@ brew install direnv
 brew install rbenv
 brew install mc
 brew install pstree
-
-# Corrects errors in previous console command
-brew install fuck
-
-# homebrew extended
-brew cask # or brew install caskroom/cask/brew-cask
-
-# iterm2 <3
+brew install mkcert # locally-trusted development certificates
+brew install nss # mkcert->firefox support
+brew install fuck # (corrects errors in previous console command)
+brew cask # or brew install caskroom/cask/brew-cask (homebrew extended)
 brew cask install iterm2 && brew cask cleanup
-
-# Archiver
-brew cask install keka
-
-# Right browser in right place
-brew cask install finicky
-
-# Documentation generator
-pip install mkdocs
+brew cask install keka # (archiver)
+brew cask install finicky # (right browser in right place)
+pip install mkdocs # documentation generator
 
 # Zsh Prompt
 yarn global add spaceship-prompt
