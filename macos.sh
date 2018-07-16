a99e7ec958536dcf449479f014e356807039a1b5
Rustam Ibragimov <iamdaiver@icloud.com>
2018-07-16 23:29:09 +0300

    [macos] browsers, vm, api tets utility, git view, IMAGEMAGICK ARGH

--- macos.sh
+++ macos.sh
@@ -19,11 +19,17 @@ brew install mkcert # locally-trusted development certificates
 brew install nss # mkcert->firefox support
 brew install fuck # (corrects errors in previous console command)
 brew install watchman
+brew install imagemagick # (ARGH AAARGGHHHHH)
 brew cask # or brew install caskroom/cask/brew-cask (homebrew extended)
 brew cask install iterm2 && brew cask cleanup
 brew cask install keka # (archiver)
 brew cask install finicky # (right browser in right place)
 brew cask install sonic-pi # (<3 live-coding virtual synth)
+brew cask install google-chrome
+brew cask install firefox
+brew cask install postman # (api test utility)
+brew cask install virtualbox
+brew cask install sourcetree
 pip install mkdocs # documentation generator
 
 # Zsh Prompt
