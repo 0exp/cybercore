f3028315f53753bc61a892f4cdc42958e848e006
Rustam Ibragimov <iamdaiver@gmail.com>
2019-09-13 18:13:37 +0300

    [macos] genpasswd utility

--- dotfiles/.zsh/functions.sh
+++ dotfiles/.zsh/functions.sh
@@ -1,5 +1,11 @@
 # works with <pwgen> console utility (see macos_soft ansible tasks)
 
-genpass() {
-  pwgen -Bs $1 1 | pbcopy | pbpaste
+genpasswd() {
+  local pass_length=12
+
+  if [ ! -z "$1" ]; then
+    local pass_length=$1
+  fi
+
+  pwgen -Bs $pass_length 1 | pbcopy | pbpaste;
 }
