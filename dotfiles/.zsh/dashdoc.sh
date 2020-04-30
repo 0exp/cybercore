188188996ae7fff0649a7a0408946ef160c87267
Rustam Ibragimov <iamdaiver@gmail.com>
2020-04-30 18:24:14 +0300

    [sh] dashdoc aliases for pg and rails

--- dotfiles/.zsh/dashdoc.sh
+++ dotfiles/.zsh/dashdoc.sh
@@ -17,3 +17,23 @@ rubydash() {
 
   dashdoc ruby:${search_pattern}
 }
+
+pgdash() {
+  local search_pattern=""
+
+  if [ ! -z "$1" ]; then
+    local search_pattern=$1
+  fi
+
+  dashdoc psql:${search_pattern}
+}
+
+railsdash() {
+  local search_pattern=""
+
+  if [ ! -z "$1" ]; then
+    local search_pattern=$1
+  fi
+
+  dashdoc rails:${search_pattern}
+}
